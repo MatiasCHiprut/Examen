1be62f26c39fbacb4fe86f61d76eedffcb56302e
MatiasChiprut <92793353+MatiasCHiprut@users.noreply.github.com>
2022-04-11 11:21:40 -0300

    Initial commit

---
+++ Assets/Scripts/Ej1.cs
@@ -0,0 +1,50 @@
+﻿//Realizar el programa que permita ingresar por inspector el código de la categoría del animal y cuántos días va a quedarse.
+//El programa debe cumplir los siguientes requerimientos:
+//Debe devolver el mensaje “Para ese período se necesitan  ” + [peso en gramos del alimento] + “ gramos de alimento”.
+//Sabiendo que el precio del alimento es de $80 por 100g, agregar un mensaje que muestre el costo de la cantidad de alimento calculada.
+//Debe mostrar mensajes de error(y no realizar el cálculo) si se ingresa un código no válido o si se ingresa una cantidad de días menor a 3.
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour
+{
+    public string codigo;
+    public int dias;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (codigo == G)
+        {
+            int total = 300 * dias;
+            Debug.Log("Para ese período se necesitan "+total+" gramos de alimento");
+        }
+        else if (codigo == PP)
+        {
+            int total = 400 * dias;
+            Debug.Log("Para ese período se necesitan "+total +" gramos de alimento");
+        }
+        else if (codigo == PG)
+        {
+            int total = 700 * dias;
+            Debug.Log("Para ese período se necesitan "+total+" gramos de alimento");
+        }
+        else if (dias < 3)
+        {
+            Debug.Log("Error");
+        }
+        else
+        {
+            Debug.Log("Error");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
